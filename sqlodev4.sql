b28164d28d578725c1eac62af2405be1f168cbfb
yildirimog8 <100138219+yildirimog8@users.noreply.github.com>
2022-07-30 18:04:01 +0300

    Create sqlodev4.sql

---
+++ sqlodev4.sql
@@ -0,0 +1,9 @@
+1--SELECT DISTINCT replacement_cost FROM film;
+2--SELECT DISTINCT(COUNT(replacement_cost)) FROM film;
+3--SELECT COUNT(title) from film 
+WHERE title LIKE'T%' and rating='G';
+
+4--SELECT COUNT (*) FROM country WHERE country LIKE '_____';
+ 
+5-- select count(*) from city
+where city ilike 'R%r';
