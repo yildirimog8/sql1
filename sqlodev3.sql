6d76ee1e16a29ecb40e13ea162f19e0910ba3858
yildirimog8 <100138219+yildirimog8@users.noreply.github.com>
2022-07-30 17:27:18 +0300

    Create sqlodev3.sql

---
+++ sqlodev3.sql
@@ -0,0 +1,5 @@
+SELECT * FROM Country WHERE country LIKE 'A%a';
+SELECT * FROM Country WHERE country LIKE '_____n';
+SELECT title FROM film WHERE title ILIKE '%T%T%T%T%';
+SELECT * FROM film WHERE title LIKE '%C' AND length > 90 AND rental_rate = 2.99;
+ 
