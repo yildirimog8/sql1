0339800eb7c27ddd79c6e5edf8030705a5a508e3
yildirimog8 <100138219+yildirimog8@users.noreply.github.com>
2022-07-31 20:33:57 +0300

    Create sqlodev7.sql

---
+++ sqlodev7.sql
@@ -0,0 +1,19 @@
+--HAVING anahtar kelimesi ile gruplanmış verilere koşul koşabiliriz.
+--1
+SELECT rating FROM film
+GROUP BY rating;
+
+--2
+SELECT replacement_cost,COUNT(*) FROM film
+GROUP BY replacement_cost
+HAVING COUNT(*) > 50;
+
+--3
+SELECT store_id,COUNT(*) FROM customer
+GROUP BY store_id;
+
+--4
+SELECT country_id,COUNT(*) FROM city
+GROUP BY country_id
+ORDER BY COUNT(city)DESC
+LIMIT 1;
