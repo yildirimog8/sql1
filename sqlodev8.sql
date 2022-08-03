ee4c4c88be272fe20b5e5a147f16bcd27d581940
yildirimog8 <100138219+yildirimog8@users.noreply.github.com>
2022-08-03 20:40:49 +0300

    Create sqlodev8.sql

---
+++ sqlodev8.sql
@@ -0,0 +1,115 @@
+--GİRİŞ
+CREATE TABLE employee(
+    ID SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    birthday DATE,
+    eposta VARCHAR(100)   
+)
+
+2--
+insert into employee (name, birthday, eposta) values ('Petronille Sidnall', '2021-10-28', 'psidnall0@geocities.jp');
+insert into employee (name, birthday, eposta) values ('Sydney Zywicki', '2021-10-14', 'szywicki1@google.de');
+insert into employee (name, birthday, eposta) values ('Rodger Lathaye', '2021-12-07', 'rlathaye2@usnews.com');
+insert into employee (name, birthday, eposta) values ('Ross Thirtle', '2022-05-14', 'rthirtle3@canalblog.com');
+insert into employee (name, birthday, eposta) values ('Goldia Bingall', '2022-05-25', 'gbingall4@stanford.edu');
+insert into employee (name, birthday, eposta) values ('Felicdad McGahey', '2021-12-04', 'fmcgahey5@chicagotribune.com');
+insert into employee (name, birthday, eposta) values ('Alejoa Ansill', null, null);
+insert into employee (name, birthday, eposta) values ('Arte Shank', '2021-12-29', 'ashank7@google.de');
+insert into employee (name, birthday, eposta) values ('Emalia Dodgshun', '2021-10-02', 'edodgshun8@nps.gov');
+insert into employee (name, birthday, eposta) values ('Leonid Cadman', '2022-03-21', 'lcadman9@webeden.co.uk');
+insert into employee (name, birthday, eposta) values ('Russ O''Deoran', '2021-11-12', 'rodeorana@reverbnation.com');
+insert into employee (name, birthday, eposta) values ('Jakob Mellsop', '2022-03-13', 'jmellsopb@exblog.jp');
+insert into employee (name, birthday, eposta) values ('Prue Manville', '2022-06-16', 'pmanvillec@e-recht24.de');
+insert into employee (name, birthday, eposta) values ('Felicity Olijve', '2021-09-13', 'folijved@dailymotion.com');
+insert into employee (name, birthday, eposta) values ('Bradly Swindley', null, null);
+insert into employee (name, birthday, eposta) values ('Molly Kender', '2021-10-16', 'mkenderf@hhs.gov');
+insert into employee (name, birthday, eposta) values ('Blithe Tebbitt', '2022-04-25', 'btebbittg@artisteer.com');
+insert into employee (name, birthday, eposta) values ('Whitby Cohn', '2021-12-24', 'wcohnh@surveymonkey.com');
+insert into employee (name, birthday, eposta) values ('Isak Theakston', '2022-03-13', 'itheakstoni@vk.com');
+insert into employee (name, birthday, eposta) values ('Evangelina Jenton', '2021-10-10', 'ejentonj@ftc.gov');
+insert into employee (name, birthday, eposta) values ('Ermanno Sutherel', '2022-07-06', 'esutherelk@dell.com');
+insert into employee (name, birthday, eposta) values ('Kin Ramsdale', '2022-02-15', 'kramsdalel@ibm.com');
+insert into employee (name, birthday, eposta) values ('Feliks Iacovielli', '2022-05-16', 'fiacoviellim@abc.net.au');
+insert into employee (name, birthday, eposta) values ('Gerhardine Proudman', '2022-05-23', 'gproudmann@accuweather.com');
+insert into employee (name, birthday, eposta) values ('Kaylee Lippingwell', '2021-10-23', 'klippingwello@t-online.de');
+insert into employee (name, birthday, eposta) values ('Rebekah Pleasants', '2022-04-28', 'rpleasantsp@google.pl');
+insert into employee (name, birthday, eposta) values ('Grannie Stairs', '2022-01-30', 'gstairsq@wikia.com');
+insert into employee (name, birthday, eposta) values ('Teodoro Laurentino', '2022-04-10', 'tlaurentinor@ocn.ne.jp');
+insert into employee (name, birthday, eposta) values ('Christopher Moorrud', null, null);
+insert into employee (name, birthday, eposta) values ('Sharline Montgomery', '2022-02-26', 'smontgomeryt@pbs.org');
+insert into employee (name, birthday, eposta) values ('Zacharias Swiffen', '2022-02-10', 'zswiffenu@google.ru');
+insert into employee (name, birthday, eposta) values ('Korella Ades', null, null);
+insert into employee (name, birthday, eposta) values ('Whitby Collier', '2022-03-20', 'wcollierw@fc2.com');
+insert into employee (name, birthday, eposta) values ('Rolando Waine', '2022-04-25', 'rwainex@bbb.org');
+insert into employee (name, birthday, eposta) values ('Hank Maun', '2021-11-30', 'hmauny@seattletimes.com');
+insert into employee (name, birthday, eposta) values ('Nanine Loomis', '2022-07-04', 'nloomisz@wordpress.org');
+insert into employee (name, birthday, eposta) values ('Malissa Dowzell', null, null);
+insert into employee (name, birthday, eposta) values ('Bunny Jiggens', '2021-10-12', 'bjiggens11@xrea.com');
+insert into employee (name, birthday, eposta) values ('Eleonora Hazelby', '2021-10-20', 'ehazelby12@telegraph.co.uk');
+insert into employee (name, birthday, eposta) values ('Marie-jeanne Kienlein', '2021-12-10', 'mkienlein13@amazon.com');
+insert into employee (name, birthday, eposta) values ('Falkner Ransome', '2021-12-31', 'fransome14@vk.com');
+insert into employee (name, birthday, eposta) values ('Lisette O''Hear', '2022-01-25', 'lohear15@nifty.com');
+insert into employee (name, birthday, eposta) values ('Darin Hymus', '2021-08-20', 'dhymus16@google.com.au');
+insert into employee (name, birthday, eposta) values ('Mickey Petrozzi', '2022-06-01', 'mpetrozzi17@prweb.com');
+insert into employee (name, birthday, eposta) values ('Flinn Follen', '2022-04-07', 'ffollen18@mtv.com');
+insert into employee (name, birthday, eposta) values ('Charissa Grundell', '2022-01-17', 'cgrundell19@constantcontact.com');
+insert into employee (name, birthday, eposta) values ('Hildagarde Velde', '2022-05-18', 'hvelde1a@i2i.jp');
+insert into employee (name, birthday, eposta) values ('Alexis Alderwick', '2021-11-27', 'aalderwick1b@netscape.com');
+insert into employee (name, birthday, eposta) values ('Rasia Boag', '2022-06-13', 'rboag1c@craigslist.org');
+insert into employee (name, birthday, eposta) values ('Vale Noon', '2022-01-20', 'vnoon1d@cnet.com');
+
+
+3--
+UPDATE employee 
+    SET name = 'Oğuzhan YILDIRIM'
+    WHERE ID = 6;
+    
+UPDATE employee 
+    SET name = 'Kağan YILDIRIM'
+    WHERE ID = 26;
+    
+    
+UPDATE employee 
+    SET name = 'bkagan@gmail.com'
+    WHERE ID = 26;
+    
+UPDATE employee
+  SET birthday = '1919-05-19'
+  WHERE id=19;
+  
+UPDATE employee 
+    SET eposta = '1231'
+    WHERE ID = 36;
+    
+    
+--------4
+delete from employee 
+where name='Darin Hymus';
+-- Silme işleminden sonraki tabloyu görüntüleyelim
+select * from employee;
+
+  --2.silme işlemi
+delete from employee 
+where email like '%u';
+-- Silme işleminden sonraki tabloyu görüntüleyelim
+select * from employee;
+
+  -- 3.silme işlemi
+delete from employee 
+where name like 'K%';
+-- Silme işleminden sonraki tabloyu görüntüleyelim
+select * from employee;
+
+  -- 4.silme işlemi
+delete from employee 
+where id in(1,2,8,18,40);
+-- Silme işleminden sonraki tabloyu görüntüleyelim
+select * from employee;
+
+  -- 5.silme işlemi
+delete from employee 
+where id between 10 and 20;
+-- Silme işleminden sonraki tabloyu görüntüleyelim
+select * from employee;
+
+
